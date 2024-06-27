0321391ece74b84233bedbeca7fc5050c8e9fcde
javieralmeida30 <javieralmeida@upshow.tv>
2024-06-27 15:29:43 -0300

    navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Inicio</Link>
-          </li>
-          <li>
-            <Link to="/Documentación">Documentación</Link>
-          </li>
-          <li>
-            <Link to="/exams">Exámenes de prueba</Link>
-          </li>
-          <li>
-            <Link to="/tips">Consejos y Asociaciones</Link>
-          </li>
-          <li>
-            <Link to="/proyectos">Proyectos</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contacto</Link>
-          </li>
-        </ul>
-      </nav>
+        <nav>
+            <div className="nav-container">
+                <div className="nav-toggle" onClick={toggleMenu}>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </div>
+                <div className={`nav-links ${isOpen ? 'open' : ''}`}>
+                    <ul>
+                        <li>
+                            <Link to="/" onClick={toggleMenu}>Inicio</Link>
+                        </li>
+                        <li>
+                            <Link to="/Documentación" onClick={toggleMenu}>Documentación</Link>
+                        </li>
+                        <li>
+                            <Link to="/exams" onClick={toggleMenu}>Exámenes de prueba</Link>
+                        </li>
+                        <li>
+                            <Link to="/tips" onClick={toggleMenu}>Consejos y Asociaciones</Link>
+                        </li>
+                        <li>
+                            <Link to="/proyectos" onClick={toggleMenu}>Proyectos</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact" onClick={toggleMenu}>Contacto</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
 
